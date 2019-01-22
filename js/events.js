f7dff6b7ac31e7a6bf796a2e61b6079c61a8073c
Patricia Nykaza <pnykaza@calstatela.edu>
2019-01-22 19:31:51 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,4 +1,32 @@
 //define functions here
+function getIt() {
+  $("p").on("click", function() {
+    alert("Hey!")
+    return
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function(){
+    $( "img" ).addClass( "tasty" )
+    return
+  })
+}
+
+function pressIt() {
+  $('input').on('keydown', function(key) {
+  if(key.which == 71){
+      alert('g was pressed');
+  }
+})
+}
+
+function submitIt() {
+  $("form").on("submit", function() {
+    alert('Your form is going to be submitted now.');
+    return;
+})
+}
 
 $(document).ready(function(){
 

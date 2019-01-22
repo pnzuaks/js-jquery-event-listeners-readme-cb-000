d35d1eeed78fa76422acf0fc10030d2deddbef34
Patricia Nykaza <pnykaza@calstatela.edu>
2019-01-22 19:31:39 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -23,10 +23,8 @@ function pressIt() {
 
 function submitIt() {
   $("form").on("submit", function() {
-  if ($( "input:first" ).val() === "correct") {
     alert('Your form is going to be submitted now.');
     return;
-  }
 })
 }
 
